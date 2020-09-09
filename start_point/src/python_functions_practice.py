c1c5fb457796741ffa33c66a915e70d3d1d55cd1
emmikoivisto <emetsi@gmail.com>
2020-09-09 16:47:05 +0100

    tests added

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,25 @@
+def return_10():
+    return 10
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def length_of_string(banana):
+    return len(banana)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(str1, str2):
+    return int(str1) + int(str2)
+
+def number_to_full_month_name()
